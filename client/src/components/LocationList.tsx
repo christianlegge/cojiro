4a21d958ce7a582a362e93cd35936e244c103dd6
scatter-dev <christian@christianlegge.dev>
2022-08-30 01:57:16 -0400

    fixed some item names

--- client/src/components/LocationList.tsx
+++ client/src/components/LocationList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import regions from "../helpers/regions";
 import Playthrough from "../contexts/Playthrough";
 import axios from "axios";
@@ -28,33 +28,46 @@ const LocationList = ({
 	allLocations: string[];
 }) => {
 	const playthroughId = useContext(Playthrough);
+	const [lastItem, setLastItem] = useState("");
 	return (
-		<div className="flex flex-wrap gap-2">
-			{Object.keys(regions[region].locations)
-				.filter((el) => allLocations.includes(el))
-				.map((el) => (
-					<button
-						className={`block rounded-md p-2 ${
-							checked.includes(el)
-								? "line-through cursor-default bg-gray-100 text-gray-500"
-								: "shadow-md cursor-pointer bg-green-200"
-						}`}
-						key={el}
-						onClick={async () => {
-							let res = await axios.get(
-								`${process.env.REACT_APP_SERVER_URL}/checkLocation`,
-								{ params: { id: playthroughId, location: el } }
-							);
-							if (res.status === 200) {
-								setItems((items) => [...items, res.data]);
-								setChecked([...checked, el]);
-							}
-						}}
-					>
-						{locationDisplayName(el, region)}
-					</button>
-				))}
-		</div>
+		<>
+			<span className="text-2xl mx-auto">{lastItem}</span>
+			<div className="flex flex-wrap gap-2">
+				{Object.keys(regions[region].locations)
+					.filter((el) => allLocations.includes(el))
+					.map((el) => (
+						<button
+							className={`block rounded-md p-2 ${
+								checked.includes(el)
+									? "line-through cursor-default bg-gray-100 text-gray-500"
+									: "shadow-md cursor-pointer bg-green-200"
+							}`}
+							key={el}
+							onClick={async () => {
+								if (checked.includes(el)) {
+									return;
+								}
+								let res = await axios.get(
+									`${process.env.REACT_APP_SERVER_URL}/checkLocation`,
+									{
+										params: {
+											id: playthroughId,
+											location: el,
+										},
+									}
+								);
+								if (res.status === 200) {
+									setItems((items) => [...items, res.data]);
+									setChecked([...checked, el]);
+									setLastItem(res.data);
+								}
+							}}
+						>
+							{locationDisplayName(el, region)}
+						</button>
+					))}
+			</div>
+		</>
 	);
 };
 
