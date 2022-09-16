e39df2815ce254609af4b3b6cb802862260d60e8
scatter-dev <christian@christianlegge.dev>
2022-09-16 15:00:17 -0400

    smaller regions on small screens

--- client/src/components/RegionList.tsx
+++ client/src/components/RegionList.tsx
@@ -30,7 +30,7 @@ const RegionList = ({
 					.map((el) => (
 						<div
 							key={el}
-							className={`p-2 border-y-2 text-white lg:text-right lg:w-full w-32 h-32 border-2 lg:h-auto transition ${
+							className={`px-4 py-2 border-y-2 text-white lg:text-right lg:w-full border-2 lg:h-auto transition ${
 								el === region
 									? "font-semibold bg-zinc-500 scale-110 z-20 lg:translate-x-4"
 									: "hover:bg-zinc-600 active:bg-zinc-800 bg-zinc-700 cursor-pointer hover:z-10 hover:scale-105 hover:lg:scale-100 hover:lg:translate-x-2"
