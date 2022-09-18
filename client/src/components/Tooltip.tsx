dc6b52946621e2f9c19715c8611e24c44d5c67e4
Christian Legge <christian@christianlegge.dev>
2022-09-18 00:09:04 -0400

    fixed tooltips behind items

--- client/src/components/Tooltip.tsx
+++ client/src/components/Tooltip.tsx
@@ -15,7 +15,7 @@ const Tooltip = ({
 	showInfoIcon?: boolean;
 }) => {
 	return (
-		<div className={`${className} group`} style={style}>
+		<div className={`${className} group hover:z-50`} style={style}>
 			<div className="bottom-full absolute w-max max-w-[20rem] font-semibold scale-0 group-hover:scale-100 text-white bg-zinc-900 p-2 rounded-md order-last transition duration-100 z-50">
 				{content}
 			</div>
