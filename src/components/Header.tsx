a46934425445267c595414b7ef43fa398b373334
scatter-dev <christian@christianlegge.dev>
2022-09-23 20:01:36 -0400

    auth status and logout in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+	const { data: session, status } = useSession();
+
+	console.log("data?", session);
+	console.log("status?", status);
+
 	const activeClass = "text-red-500";
 
 	return (
-		<header className="w-full h-20 bg-gray-700 text-gray-50 text-center">
+		<header className="w-full h-20 px-4 bg-zinc-700 text-gray-50 flex justify-between items-center">
+			<div>logo</div>
 			<div className="flex h-full justify-center items-center gap-12">
 				<Link href="/play">Play</Link>
 				<Link href="/how-to-play">How to Play</Link>
-				<Link href="/login">Log in/Register</Link>
+			</div>
+			<div className="flex gap-2 items-center">
+				{status === "unauthenticated" && (
+					<Link href="/login">Log in</Link>
+				)}
+				{status === "authenticated" && (
+					<>
+						{session.user?.name}{" "}
+						<img
+							src={session.user?.image as string}
+							className="h-10 rounded-full"
+						/>
+						<button onClick={() => signOut()}>Log out</button>
+					</>
+				)}
 			</div>
 		</header>
 	);
