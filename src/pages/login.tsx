b2bdd6cf094ba7f6b79c6a7c3222cbb00d91b0bf
Christian Legge <christian@christianlegge.dev>
2022-09-24 22:49:02 -0400

    removed calls to removed trpc endpoint

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,110 +21,112 @@ const RegisterForm = () => {
 		resolver: zodResolver(registerValidation),
 	});
 
-	const usernameExists = trpc.useMutation("user.usernameExists", {
-		onSuccess(data) {
-			if (data) {
-				setError("username", { message: "Username already exists :(" });
-			}
-		},
-	});
+	return <div>Unimplemented.</div>;
 
-	const registerMutation = trpc.useMutation("user.register", {
-		onError(error, variables, context) {
-			console.log("error");
-			console.log(error.message);
-			if (error.data?.code === "CONFLICT") {
-				setError(error.message, {
-					message: `User with ${error.message} already exists`,
-				});
-			}
-		},
-	});
+	// const usernameExists = trpc.useMutation("user.usernameExists", {
+	// 	onSuccess(data) {
+	// 		if (data) {
+	// 			setError("username", { message: "Username already exists :(" });
+	// 		}
+	// 	},
+	// });
 
-	return (
-		<form
-			className="flex flex-col"
-			onSubmit={handleSubmit(({ email, username, password }) =>
-				registerMutation.mutate({ email, username, password })
-			)}
-		>
-			<div>
-				<label htmlFor="email">email</label>
-				<input
-					{...register("email")}
-					className={`${textInputClasses} ${
-						errors.email && "border-red-500"
-					}`}
-					type="text"
-					name="email"
-					id="email"
-					placeholder="email"
-				/>
-				<p
-					className={`text-sm ${
-						errors.email ? "visible" : "invisible"
-					}`}
-				>
-					{errors.email
-						? (errors.email.message as string)
-						: "No errors"}
-				</p>
-			</div>
-			<div>
-				<label htmlFor="username">username</label>
-				<input
-					{...register("username")}
-					className={`${textInputClasses} ${
-						errors.username && "border-red-500"
-					}`}
-					type="text"
-					name="username"
-					id="username"
-					placeholder="username"
-					onBlur={(e) => usernameExists.mutate(e.currentTarget.value)}
-				/>
-				<p
-					className={`text-sm ${
-						errors.username ? "visible" : "invisible"
-					}`}
-				>
-					{errors.username
-						? (errors.username.message as string)
-						: "No errors"}
-				</p>
-			</div>
-			<div>
-				<label htmlFor="password">password</label>
-				<input
-					{...register("password")}
-					className={`${textInputClasses} ${
-						errors.password && "border-red-500"
-					}`}
-					type="password"
-					name="password"
-					id="password"
-					placeholder="password"
-				/>
-				<p
-					className={`text-sm ${
-						errors.password ? "visible" : "invisible"
-					}`}
-				>
-					{errors.password
-						? (errors.password.message as string)
-						: "No errors"}
-				</p>
-			</div>
+	// const registerMutation = trpc.useMutation("user.register", {
+	// 	onError(error, variables, context) {
+	// 		console.log("error");
+	// 		console.log(error.message);
+	// 		if (error.data?.code === "CONFLICT") {
+	// 			setError(error.message, {
+	// 				message: `User with ${error.message} already exists`,
+	// 			});
+	// 		}
+	// 	},
+	// });
 
-			<button
-				className={`${
-					isValid ? "bg-indigo-700" : "cursor-default bg-zinc-500"
-				} w-full rounded-lg  text-white py-2 mt-4`}
-			>
-				submit
-			</button>
-		</form>
-	);
+	// return (
+	// 	<form
+	// 		className="flex flex-col"
+	// 		onSubmit={handleSubmit(({ email, username, password }) =>
+	// 			registerMutation.mutate({ email, username, password })
+	// 		)}
+	// 	>
+	// 		<div>
+	// 			<label htmlFor="email">email</label>
+	// 			<input
+	// 				{...register("email")}
+	// 				className={`${textInputClasses} ${
+	// 					errors.email && "border-red-500"
+	// 				}`}
+	// 				type="text"
+	// 				name="email"
+	// 				id="email"
+	// 				placeholder="email"
+	// 			/>
+	// 			<p
+	// 				className={`text-sm ${
+	// 					errors.email ? "visible" : "invisible"
+	// 				}`}
+	// 			>
+	// 				{errors.email
+	// 					? (errors.email.message as string)
+	// 					: "No errors"}
+	// 			</p>
+	// 		</div>
+	// 		<div>
+	// 			<label htmlFor="username">username</label>
+	// 			<input
+	// 				{...register("username")}
+	// 				className={`${textInputClasses} ${
+	// 					errors.username && "border-red-500"
+	// 				}`}
+	// 				type="text"
+	// 				name="username"
+	// 				id="username"
+	// 				placeholder="username"
+	// 				onBlur={(e) => usernameExists.mutate(e.currentTarget.value)}
+	// 			/>
+	// 			<p
+	// 				className={`text-sm ${
+	// 					errors.username ? "visible" : "invisible"
+	// 				}`}
+	// 			>
+	// 				{errors.username
+	// 					? (errors.username.message as string)
+	// 					: "No errors"}
+	// 			</p>
+	// 		</div>
+	// 		<div>
+	// 			<label htmlFor="password">password</label>
+	// 			<input
+	// 				{...register("password")}
+	// 				className={`${textInputClasses} ${
+	// 					errors.password && "border-red-500"
+	// 				}`}
+	// 				type="password"
+	// 				name="password"
+	// 				id="password"
+	// 				placeholder="password"
+	// 			/>
+	// 			<p
+	// 				className={`text-sm ${
+	// 					errors.password ? "visible" : "invisible"
+	// 				}`}
+	// 			>
+	// 				{errors.password
+	// 					? (errors.password.message as string)
+	// 					: "No errors"}
+	// 			</p>
+	// 		</div>
+
+	// 		<button
+	// 			className={`${
+	// 				isValid ? "bg-indigo-700" : "cursor-default bg-zinc-500"
+	// 			} w-full rounded-lg  text-white py-2 mt-4`}
+	// 		>
+	// 			submit
+	// 		</button>
+	// 	</form>
+	// );
 };
 
 const LoginRegister = () => {
