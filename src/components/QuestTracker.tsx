dcd0f1c19e80c450665f6ebb93bf00d0ab9eaeb1
scatter-dev <christian@christianlegge.dev>
2023-01-10 01:00:49 -0500

    added skull counter

--- src/components/QuestTracker.tsx
+++ src/components/QuestTracker.tsx
@@ -18,28 +18,49 @@ const QuestTracker: React.FC<{
 	const stones = ["Kokiri Emerald", "Goron Ruby", "Zora Sapphire"];
 
 	return (
-		<div className="w-52">
-			<MedallionCircle items={items} itemLocations={itemLocations} />
-			<div className="mt-20 flex w-full justify-between gap-1">
-				{stones.map((stone) => (
-					<Tooltip
-						key={stone}
-						content={
-							stone in itemLocations
-								? `${stone} (${itemLocations[stone]})`
-								: stone
-						}
-						className="relative h-20 w-20"
-						showInfoIcon={stone in itemLocations}
-					>
-						<ItemIcon
-							src={`/images/${formatFilename(stone)}.png`}
-							className="h-full w-full object-contain"
-							alt={stone}
-							has={items.includes(stone)}
-						/>
-					</Tooltip>
-				))}
+		<div className="flex items-center gap-4">
+			<div className="w-52">
+				<MedallionCircle items={items} itemLocations={itemLocations} />
+				<div className="mt-20 flex w-full justify-between gap-1">
+					{stones.map((stone) => (
+						<Tooltip
+							key={stone}
+							content={
+								stone in itemLocations
+									? `${stone} (${itemLocations[stone]})`
+									: stone
+							}
+							className="relative h-20 w-20"
+							showInfoIcon={stone in itemLocations}
+						>
+							<ItemIcon
+								src={`/images/${formatFilename(stone)}.png`}
+								className="h-full w-full object-contain"
+								alt={stone}
+								has={items.includes(stone)}
+							/>
+						</Tooltip>
+					))}
+				</div>
+			</div>
+			<div>
+				<Tooltip
+					content="Gold Skulltula Token"
+					className="relative h-20 w-20"
+				>
+					<ItemIcon
+						src="/images/skulltula.png"
+						alt="Gold Skulltula Token"
+						has={items.includes("Gold Skulltula Token")}
+						className="h-full w-full object-contain"
+					/>
+				</Tooltip>
+				<p className="text-center text-3xl font-semibold text-white">
+					{
+						items.filter((item) => item === "Gold Skulltula Token")
+							.length
+					}
+				</p>
 			</div>
 		</div>
 	);
