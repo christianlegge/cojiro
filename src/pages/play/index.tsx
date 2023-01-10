300903468eb86644b6839073c4152ca52135361a
scatter-dev <christian@christianlegge.dev>
2023-01-09 22:35:01 -0500

    removed sample seed button

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -293,13 +293,6 @@ const StartForm = () => {
 						)}
 					</div>
 					<ErrorBox error={error} />
-					<button
-						onClick={() =>
-							startMutation.mutate({ sampleSeed: true })
-						}
-					>
-						Sample seed
-					</button>
 					<h2>In progress games</h2>
 					<ul className="flex max-w-4xl flex-wrap justify-center gap-4">
 						{inProgressPlaythroughs.length === 0 ? (
