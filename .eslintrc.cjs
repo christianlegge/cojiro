faf7d66b9c18c2a0f9bb2b12cf96c95ef6f87b5a
Christian Legge <christian@christianlegge.dev>
2023-11-09 10:58:33 -0500

    fixed ts setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+	},
+	plugins: ["@typescript-eslint"],
+	extends: [
+		"next/core-web-vitals",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"prettier",
+	],
+};
+
+module.exports = config;
